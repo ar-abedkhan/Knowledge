201bb843b6f551a41c51368f9805aba108bc8505
Osama Bin Hashim <zeeshan2k01@gmail.com>
2022-12-12 23:12:17 +0600

    login and registration design

---
+++ app/src/main/java/com/abedkhan/knowledge/Activities/AdminLoginActivity.java
@@ -0,0 +1,16 @@
+package com.abedkhan.knowledge.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.abedkhan.knowledge.R;
+
+public class AdminLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+    }
+}
