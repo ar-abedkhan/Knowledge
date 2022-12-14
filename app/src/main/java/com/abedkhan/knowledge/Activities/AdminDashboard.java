170ea22df6f9fc8ac9f9beed61abf596e26387f9
ar-abedkhan <abedkhan783@gmail.com>
2022-12-14 19:56:58 +0600

    edit fragment design

--- app/src/main/java/com/abedkhan/knowledge/Activities/AdminDashboard.java
+++ app/src/main/java/com/abedkhan/knowledge/Activities/AdminDashboard.java
@@ -4,18 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.abedkhan.knowledge.Fragments.AddFragment;
 import com.abedkhan.knowledge.MainActivity;
 import com.abedkhan.knowledge.R;
+import com.abedkhan.knowledge.databinding.ActivityAdminDashboardBinding;
 
 public class AdminDashboard extends AppCompatActivity {
+ActivityAdminDashboardBinding binding;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_dashboard);
-    }
+        binding=ActivityAdminDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+
+        binding.addOption.setOnClickListener(view -> {
+binding.dashboard.setVisibility(View.GONE);
+            getSupportFragmentManager().beginTransaction().add(R.id.fragmentView,new AddFragment()).commit();
+
+        });
+
+    }
 
 
 
---
+++ app/src/main/java/com/abedkhan/knowledge/Fragments/AddFragment.java
@@ -0,0 +1,35 @@
+package com.abedkhan.knowledge.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.abedkhan.knowledge.R;
+public class AddFragment extends Fragment {
+
+    public AddFragment() {
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+}
