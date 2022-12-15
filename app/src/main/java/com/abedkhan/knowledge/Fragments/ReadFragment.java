89545bec15f168c14ab94c0305d36935786315ca
Osama Bin Hashim <zeeshan2k01@gmail.com>
2022-12-15 16:52:20 +0600

    Room Database added..

--- app/src/main/java/com/abedkhan/knowledge/Fragments/ReadFragment.java
+++ app/src/main/java/com/abedkhan/knowledge/Fragments/ReadFragment.java
@@ -2,6 +2,7 @@ package com.abedkhan.knowledge.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +12,12 @@ import android.view.ViewGroup;
 import com.abedkhan.knowledge.Adapters.ChapterAdapter;
 import com.abedkhan.knowledge.Modelclass.ChapterModelClass;
 import com.abedkhan.knowledge.databinding.FragmentReadBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +29,9 @@ public class ReadFragment extends Fragment {
     }
     FragmentReadBinding binding;
     List<ChapterModelClass> chapterModelClassList;
+    FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
+
     int chapterno;
 
 
@@ -30,6 +40,9 @@ public class ReadFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding=FragmentReadBinding.inflate(getLayoutInflater(),container,false);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
         chapterModelClassList=new ArrayList<>();
 
@@ -62,6 +75,25 @@ public class ReadFragment extends Fragment {
         binding.readRecycler.setAdapter(chapterAdapter);
 
 
+        binding.readOfflineBtn.setOnClickListener(view -> {
+
+ /*           TODO: take the subject from the firebase and save it to the room database
+            databaseReference.child(subjectName).child(currentID).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+            */
+
+
+        });
 
 
 
